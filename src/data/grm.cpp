beb5fdf63efb48c4cc167539ee8107708a85204b
r1cheu <chenrulei@cemps.ac.cn>
2025-01-09 13:23:19 +0800

    remove vcf, remove redundant file, finish array caster.

--- src/data/grm.cpp
+++ src/data/grm.cpp
@@ -1,20 +1,41 @@
 #include "chenx/data/grm.h"
 
 #include <armadillo>
+#include <cmath>
 
 namespace chenx
 {
 using arma::dvec;
 dmat ComputeGRM(dmat& genotype)
 {
-    dmat grm = genotype.t() * genotype;
-    return grm / trace(grm) * static_cast<double>(grm.n_cols);
+    if (genotype.has_nan())
+    {
+        genotype.replace(arma::datum::nan, 1.0);
+    }
+    dmat grm = genotype * genotype.t();
+    return grm / trace(grm) * static_cast<double>(grm.n_rows);
 }
 
 dmat AdditiveGrm(dmat& genotype)
 {
-    genotype.each_col() -= mean(genotype, 1);
+    if (genotype.has_nan())
+    {
+        genotype.replace(arma::datum::nan, 1.0);
+    }
+
+    genotype.each_row() -= mean(genotype, 0);
     return ComputeGRM(genotype);
+}
+
+void AddChunkGrm(dmat& genotype, dmat& grm)
+{
+    if (genotype.has_nan())
+    {
+        genotype.replace(arma::datum::nan, 1.0);
+    }
+
+    genotype.each_row() -= mean(genotype, 0);
+    grm += genotype * genotype.t();
 }
 
 dmat DomainanceGrm(dmat& genotype)
