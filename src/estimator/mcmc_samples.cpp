92e4fd4a761ce5b5659e27eef73c4c10e9aed698
r1cheu <chenrulei@cemps.ac.cn>
2025-06-08 15:55:36 +0800

    refactor: update MCMCSamples to use BayesModel instead of BayesStatus

--- src/estimator/mcmc_samples.cpp
+++ src/estimator/mcmc_samples.cpp
@@ -6,36 +6,28 @@
 namespace gelex
 {
 
-MCMCSamples::MCMCSamples(const MCMCParams& params, const BayesStatus& status, size_t n_chains)
+MCMCSamples::MCMCSamples(
+    const MCMCParams& params,
+    const BayesModel& model,
+    size_t n_chains)
     : n_records_((params.iter - params.n_burnin) / params.n_thin),
       n_chains_(n_chains)
 {
     mu_.set_size(n_records_, n_chains_);
-    if (status.fixed)
+    if (model.fixed())
     {
-        fixed_.set_size(status.fixed.coeff.n_elem, n_records_, n_chains_);
+        fixed_.set_size(
+            model.fixed()->design_mat.n_cols, n_records_, n_chains_);
     }
-
-    random_.coeffs.resize(status.random.size());
-    random_.sigmas.resize(status.random.size());
-    for (size_t i = 0; i < status.random.size(); ++i)
-    {
-        random_.coeffs[i].set_size(status.random[i].coeff.n_elem, n_records_, n_chains_);
-        random_.sigmas[i].set_size(status.random[i].sigma.n_elem, n_records_, n_chains_);
-    }
-
-    genetic_.coeffs.resize(status.genetic.size());
-    genetic_.sigmas.resize(status.genetic.size());
-    for (size_t i = 0; i < status.genetic.size(); ++i)
-    {
-        genetic_.coeffs[i].set_size(status.genetic[i].coeff.n_elem, n_records_, n_chains_);
-        genetic_.sigmas[i].set_size(status.genetic[i].sigma.n_elem, n_records_, n_chains_);
-    }
-
+    init_group(random_, model.random());
+    init_group(genetic_, model.genetic());
     residual_.set_size(n_records_, n_chains_);
 }
 
-void MCMCSamples::store(const BayesStatus& status, size_t record_idx, size_t chain_idx)
+void MCMCSamples::store(
+    const BayesStatus& status,
+    size_t record_idx,
+    size_t chain_idx)
 {
     if (record_idx >= n_records_ || chain_idx >= n_chains_)
     {
