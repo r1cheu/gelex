93e90a541c74ac72fa81c4edba955f4caf4e0517
r1cheu <chenrulei@cemps.ac.cn>
2026-01-28 10:40:42 +0800

    ♻️ refactor: replace MKL-based p-value calculation with Eigen erfc

--- src/gwas/association_test.cpp
+++ src/gwas/association_test.cpp
@@ -15,27 +15,12 @@
  */
 
 #include "gelex/gwas/association_test.h"
-#include <mkl_vml_functions.h>
 
 #include <Eigen/Dense>
+#include <unsupported/Eigen/SpecialFunctions>
 
 namespace gelex::gwas
 {
-
-namespace
-{
-
-void chi2_test(
-    Eigen::Ref<Eigen::VectorXd> wald_stats,
-    Eigen::Ref<Eigen::VectorXd> p_values)
-{
-    int n = static_cast<int>(wald_stats.size());
-    vdSqrt(n, wald_stats.data(), p_values.data());
-    cblas_dscal(n, -1.0, p_values.data(), 1);
-    vdCdfNorm(n, p_values.data(), p_values.data());
-    cblas_dscal(n, 2.0, p_values.data(), 1);
-}
-}  // namespace
 
 void wald_test(AssocInput& input, AssocOutput& output)
 {
@@ -46,6 +31,6 @@ void wald_test(AssocInput& input, AssocOutput& output)
     output.beta = (output.zt_v_inv_r.array() / output.zt_v_inv_z.array());
     output.se = (1.0 / output.zt_v_inv_z.array()).sqrt();
     output.stats = (output.beta.array() / output.se.array()).square();
-    chi2_test(output.stats, output.p_value);
+    output.p_value = (output.stats.array() * 0.5).sqrt().erfc();
 }
 }  // namespace gelex::gwas
