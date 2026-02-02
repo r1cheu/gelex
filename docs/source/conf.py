01a94d7cd66dc6ec8536751231a88d80e16a7cdc
r1cheu <chenrulei@cemps.ac.cn>
2026-02-02 16:19:44 +0800

    🔧 chore: bump version to 0.11.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = "gelex"
 copyright = "2026, RuLei Chen"
 author = "RuLei Chen"
-release = "0.10.1"
+release = "0.11.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
