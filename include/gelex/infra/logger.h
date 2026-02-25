9a4e5c730d08e15082e122f3e4a0b1e626fb50e3
r1cheu <chenrulei@cemps.ac.cn>
2026-02-25 14:11:02 +0800

    ♻️ refactor(logger): internalize formatters and strip ANSI from file sink

--- include/gelex/infra/logger.h
+++ include/gelex/infra/logger.h
@@ -20,7 +20,6 @@
 #include <memory>
 #include <string_view>
 
-#include <spdlog/formatter.h>
 #include <spdlog/logger.h>
 
 namespace gelex::logging
@@ -28,21 +27,6 @@ namespace gelex::logging
 void initialize(std::string_view output_prefix = "output");
 
 std::shared_ptr<spdlog::logger>& get();
-
-class LevelFormatter : public spdlog::formatter
-{
-   public:
-    LevelFormatter();
-
-    void format(const spdlog::details::log_msg& msg, spdlog::memory_buf_t& dest)
-        override;
-
-    [[nodiscard]] std::unique_ptr<formatter> clone() const override;
-
-   private:
-    std::unique_ptr<spdlog::formatter> info_formatter_;
-    std::unique_ptr<spdlog::formatter> default_formatter_;
-};
 }  // namespace gelex::logging
 
 #endif  // GELEX_LOGGER_H_
--- src/infra/logger.cpp
+++ src/infra/logger.cpp
@@ -25,43 +25,108 @@
 
 namespace
 {
-// Use an anonymous namespace to hide global state
 std::shared_ptr<spdlog::logger> g_logger = nullptr;
 std::once_flag g_init_flag;
+
+class LevelFormatter : public spdlog::formatter
+{
+   public:
+    LevelFormatter()
+    {
+        static constexpr std::string_view info_pattern = "%v";
+        static constexpr std::string_view default_pattern = "[%^%l%$] %v";
+
+        info_formatter_ = std::make_unique<spdlog::pattern_formatter>(
+            std::string(info_pattern));
+        default_formatter_ = std::make_unique<spdlog::pattern_formatter>(
+            std::string(default_pattern));
+    }
+
+    void format(const spdlog::details::log_msg& msg, spdlog::memory_buf_t& dest)
+        override
+    {
+        if (msg.level != spdlog::level::info)
+        {
+            default_formatter_->format(msg, dest);
+        }
+        else
+        {
+            info_formatter_->format(msg, dest);
+        }
+    }
+
+    [[nodiscard]] std::unique_ptr<spdlog::formatter> clone() const override
+    {
+        return std::make_unique<LevelFormatter>();
+    }
+
+   private:
+    std::unique_ptr<spdlog::formatter> info_formatter_;
+    std::unique_ptr<spdlog::formatter> default_formatter_;
+};
+
+// Strips ANSI CSI escape sequences (e.g. fmt::fg color codes) and bare CR
+// from msg.payload before formatting, so log files remain clean and readable.
+class StripAnsiFormatter : public spdlog::formatter
+{
+   public:
+    explicit StripAnsiFormatter(std::string pattern)
+        : pattern_(std::move(pattern)),
+          formatter_(std::make_unique<spdlog::pattern_formatter>(pattern_))
+    {
+    }
+
+    void format(const spdlog::details::log_msg& msg, spdlog::memory_buf_t& dest)
+        override
+    {
+        std::string clean;
+        clean.reserve(msg.payload.size());
+        for (std::size_t i = 0; i < msg.payload.size();)
+        {
+            // ESC + '[' marks the start of a CSI sequence; skip until final
+            // byte (0x40–0x7E per ECMA-48)
+            if (msg.payload[i] == '\x1b' && i + 1 < msg.payload.size()
+                && msg.payload[i + 1] == '[')
+            {
+                i += 2;
+                while (i < msg.payload.size()
+                       && (msg.payload[i] < 0x40 || msg.payload[i] > 0x7e))
+                {
+                    ++i;
+                }
+                if (i < msg.payload.size())
+                {
+                    ++i;
+                }
+            }
+            else if (msg.payload[i] == '\r')
+            {
+                ++i;
+            }
+            else
+            {
+                clean.push_back(msg.payload[i++]);
+            }
+        }
+
+        spdlog::details::log_msg clean_msg(msg);
+        clean_msg.payload = spdlog::string_view_t(clean.data(), clean.size());
+        formatter_->format(clean_msg, dest);
+    }
+
+    [[nodiscard]] std::unique_ptr<spdlog::formatter> clone() const override
+    {
+        return std::make_unique<StripAnsiFormatter>(pattern_);
+    }
+
+   private:
+    std::string pattern_;
+    std::unique_ptr<spdlog::pattern_formatter> formatter_;
+};
 }  // namespace
 
 namespace gelex::logging
 {
-
-LevelFormatter::LevelFormatter()
-{
-    static constexpr std::string_view info_pattern = "%v";
-    static constexpr std::string_view default_pattern = "[%^%l%$] %v";
-
-    info_formatter_ = std::make_unique<spdlog::pattern_formatter>(
-        std::string(info_pattern));
-    default_formatter_ = std::make_unique<spdlog::pattern_formatter>(
-        std::string(default_pattern));
-}
-
-void LevelFormatter::format(
-    const spdlog::details::log_msg& msg,
-    spdlog::memory_buf_t& dest)
-{
-    if (msg.level != spdlog::level::info)
-    {
-        default_formatter_->format(msg, dest);
-    }
-    else
-    {
-        info_formatter_->format(msg, dest);
-    }
-}
-
-std::unique_ptr<spdlog::formatter> LevelFormatter::clone() const
-{
-    return std::make_unique<LevelFormatter>();
-}
 
 void initialize(std::string_view output_prefix)
 {
@@ -80,7 +145,9 @@ void initialize(std::string_view output_prefix)
                 = std::make_shared<spdlog::sinks::basic_file_sink_st>(
                     log_filename, true);
             file_sink->set_level(spdlog::level::trace);
-            file_sink->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%l] %v");
+            file_sink->set_formatter(
+                std::make_unique<StripAnsiFormatter>(
+                    "[%Y-%m-%d %H:%M:%S.%e] [%l] %v"));
 
             g_logger = std::make_shared<spdlog::logger>(
                 "gelex_logger",
