efb99c192ddfb3073c4e9a97b06abe3114e6ba20
r1cheu <chenrulei@cemps.ac.cn>
2026-09-02 16:37:44 +0800

    Add SIMD genotype operations

---
+++ src/bayes/detail/genotype_kernel.h
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2026 RuLei Chen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef GELEX_BAYES_DETAIL_GENOTYPE_KERNEL_H_
+#define GELEX_BAYES_DETAIL_GENOTYPE_KERNEL_H_
+
+#include <cstddef>
+#include <cstdint>
+
+namespace gelex::bayes::detail
+{
+
+using DotKernel = double (*)(
+    const std::uint8_t* genotype_column,
+    const double* lut,
+    const double* residual,
+    std::size_t size) noexcept;
+
+using AxpyKernel = void (*)(
+    const std::uint8_t* genotype_column,
+    const double* lut,
+    double scale,
+    double* residual,
+    std::size_t size) noexcept;
+
+[[nodiscard]] auto dot_scalar(
+    const std::uint8_t* genotype_column,
+    const double* lut,
+    const double* residual,
+    std::size_t size) noexcept -> double;
+
+auto axpy_scalar(
+    const std::uint8_t* genotype_column,
+    const double* lut,
+    double scale,
+    double* residual,
+    std::size_t size) noexcept -> void;
+
+[[nodiscard]] auto dot_avx2(
+    const std::uint8_t* genotype_column,
+    const double* lut,
+    const double* residual,
+    std::size_t size) noexcept -> double;
+
+auto axpy_avx2(
+    const std::uint8_t* genotype_column,
+    const double* lut,
+    double scale,
+    double* residual,
+    std::size_t size) noexcept -> void;
+
+[[nodiscard]] auto dot_avx512(
+    const std::uint8_t* genotype_column,
+    const double* lut,
+    const double* residual,
+    std::size_t size) noexcept -> double;
+
+auto axpy_avx512(
+    const std::uint8_t* genotype_column,
+    const double* lut,
+    double scale,
+    double* residual,
+    std::size_t size) noexcept -> void;
+
+[[nodiscard]] auto supports_avx2() noexcept -> bool;
+[[nodiscard]] auto supports_avx512() noexcept -> bool;
+[[nodiscard]] auto select_dot_kernel() noexcept -> DotKernel;
+[[nodiscard]] auto select_axpy_kernel() noexcept -> AxpyKernel;
+
+}  // namespace gelex::bayes::detail
+
+#endif  // GELEX_BAYES_DETAIL_GENOTYPE_KERNEL_H_
